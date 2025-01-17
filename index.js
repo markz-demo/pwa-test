ffe9be57eeaf82955ca92bca5d24401d6c437fd4
markz-demo <markz_demo@163.com>
2025-01-17 08:16:49 +0800

    commit

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function registWorker() {
         //     return;
         // }
 
-        const registration = await navigator.serviceWorker.register(`/pwa-test/service-worker.js`, { scope: '/pwa-test' });
+        const registration = await navigator.serviceWorker.register(`/pwa-test/service-worker.js`, { scope: '/pwa-test/' });
         log('ServiceWorker registration successful with scope: ' + registration.scope);
     }
     catch (e) {
@@ -77,7 +77,7 @@ navigator.serviceWorker.addEventListener('message', function (e) {
 
 // todo
 async function subscribe() {
-    const registration = await navigator.serviceWorker.getRegistration('/pwa-test')
+    const registration = await navigator.serviceWorker.getRegistration('/pwa-test/')
     if (!registration) {
         log('cannot found registration')
         return;
@@ -118,7 +118,7 @@ async function showNotification() {
         return
     }
 
-    const registration = await navigator.serviceWorker.getRegistration('/pwa-test')
+    const registration = await navigator.serviceWorker.getRegistration('/pwa-test/')
     if (!registration) {
         log('cannot found registration, need regist ServiceWorker first.')
         return;
@@ -145,7 +145,7 @@ async function showNotification2() {
         return
     }
 
-    const registration = await navigator.serviceWorker.getRegistration('/pwa-test')
+    const registration = await navigator.serviceWorker.getRegistration('/pwa-test/')
     if (!registration) {
         log('cannot found registration, need regist ServiceWorker first.')
         return;
